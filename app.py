121a7141e07b0fab77c93585fd2f9c4c8cc125e3
Lucasark <31750882+Lucasark@users.noreply.github.com>
2019-03-04 23:13:26 -0300

    SQLAlchemy

--- app.py
+++ app.py
@@ -1,31 +1,53 @@
 from flask import Flask, jsonify, json, request, render_template
-from flask_mysqldb import MySQL
+#from flask_mysqldb import MySQL
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+#from sqlalchemy import create_engine
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_jwt_extended import (create_access_token)
 
 app = Flask(__name__)
-#app.run(debug=True)
 
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '0000'
-app.config['MYSQL_DB'] = 'form'
-#app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:0000@localhost/form'
+
 app.config['JWT_SECRET_KEY'] = 'secret'
 
-mysql = MySQL(app)
+#mysql = MySQL(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 CORS(app)
+db = SQLAlchemy(app)
 
+class Formulario(db.Model):
+    __tablename__ = 'formulario'
+    idformulario = db.Column('idformulario', db.Integer, primary_key=True)
+    nome = db.Column('nome', db.String(45))
+    idade = db.Column('idade', db.Integer)
+    registro = db.Column('registro', db.String(45))
+    email = db.Column('email', db.String(45))
+    senha = db.Column('senha', db.String(100))
+    Q1 = db.Column('Q1', db.String(20))
+    Q2 = db.Column('Q2', db.Integer)
+    Q3 = db.Column('Q3', db.String(5))
+    Q4 = db.Column('Q4', db.Integer)
+
+    def __init__(self, nome, idade, registro, email, senha, Q1, Q2, Q3, Q4):
+        print('DEBUG->CRUD')
+        self.nome = nome
+        self.idade = idade
+        self.email = email
+        self.registro = registro
+        self.senha = senha
+        self.Q1 = Q1
+        self.Q2 = Q2
+        self.Q3 = Q3
+        self.Q4 = Q4
 
 @app.route('/users/register', methods=['POST'])
 def register():
 
-    cur = mysql.connection.cursor()
     nome = request.get_json()['nome']
     idade = request.get_json()['idade']
     registro = request.get_json()['registro']
@@ -35,27 +57,11 @@ def register():
     Q2 = request.get_json()['Q2']
     Q3 = request.get_json()['Q3']
     Q4 = request.get_json()['Q4']
-
-    #DEBUG
-    #Q1 = 0
-    #Q2 = 0
-    #Q3 = 0
-    #Q4 = 0
-
     #modificado = datetime.utcnow()
 
-    cur.execute("INSERT INTO formulario(nome, idade, registro, email, senha, Q1, Q2, Q3, Q4) VALUES('" +
-                str(nome) + "','" +
-                idade + "','" +
-                str(registro) + "','" +
-                str(email) + "','" +
-                str(senha) + "','" +
-                Q1 + "','" +
-                Q2 + "','" +
-                Q3 + "','" +
-                Q4 + "')")
-
-    mysql.connection.commit()
+    cursor = Formulario(nome, idade, registro, email, senha, Q1, Q2, Q3, Q4)
+    db.session.add(cursor)
+    db.session.commit()
 
     result = {
         "nome": nome,
@@ -74,24 +80,22 @@ def register():
 
 @app.route('/users/login', methods=['POST'])
 def login():
-    cur = mysql.connection.cursor()
     registro = request.get_json()['registro']
     senha = request.get_json()['senha']
     result = ""
 
-    cur.execute("SELECT * FROM formulario where registro = '" + str(registro) + "'")
-    rv = cur.fetchone()
-
-    if bcrypt.check_password_hash(rv['senha'], senha):
+    #SELECT * FROM form WHERE user.registro = resgistro
+    user = Formulario.query.filter_by(registro=registro).first()
+    if bcrypt.check_password_hash(user.senha, senha):
         access_token = create_access_token(
-            identity={'nome': rv['nome'],
-                      'idade': rv['idade'],
-                      'registro': rv['registro'],
-                      'email': rv['email'],
-                      'Q1': rv['Q1'],
-                      'Q2': rv['Q2'],
-                      'Q3': rv['Q3'],
-                      'Q4': rv['Q4']}
+            identity={'nome': user.nome,
+                      'idade': user.idade,
+                      'registro': user.registro,
+                      'email': user.email,
+                      'Q1': user.Q1,
+                      'Q2': user.Q2,
+                      'Q3': user.Q3,
+                      'Q4': user.Q4}
         )
         result = access_token
 
